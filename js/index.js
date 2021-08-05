8585190f886b9fcf98bcbf9184e4635932c74203
kishorehari231 <85133195+kishorehari231@users.noreply.github.com>
2021-08-05 20:48:29 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function success() {
+    alert("We will confirm the details you have entered and  redirect to shipping details if successful");
+}
+function shipping() {
+    alert("We will check the details you provided and proceed with your order");
+}
